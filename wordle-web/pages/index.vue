f8727a29331ecd5872f7e3f86f5e0b5614ac71dd
PlantBrush <jrobak@ewu.edu>
2022-04-12 19:18:32 -0700

    Added button for Game Page

--- wordle-web/pages/index.vue
+++ wordle-web/pages/index.vue
@@ -73,14 +73,8 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> {{ buttonText }} </v-btn>
-          <v-btn
-            color="secondary"
-            :loading="isLoading"
-            @click="changeButtonText"
-          >
-            Change Text
-          </v-btn>
+          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
+          <v-btn color="primary" nuxt to="/game"> Game Page </v-btn>
         </v-card-actions>
       </v-card>
     </v-col>
@@ -89,21 +83,10 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import Component from 'vue-class-component'
+import Component from "vue-class-component"
 
 @Component
-export default class IndexPage extends Vue {
-  name: string = 'IndexPage'
-  buttonText: string = 'Inspire me!'
-  isLoading: boolean = false
-
-  changeButtonText() {
-    this.buttonText =
-      this.buttonText === 'Inspire me!' ? 'Inspire me again!' : 'Inspire me!'
-    this.isLoading = true
-    setTimeout(() => {
-      this.isLoading = false
-    }, 1000)
-  }
+export default class IndexPage extends Vue{
+  name: string = 'IndexPage';
 }
 </script>
