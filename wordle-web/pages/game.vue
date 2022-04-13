38a631f4b914c0f6f9a07792c9c5c1e972e20d54
PlantBrush <jrobak@ewu.edu>
2022-04-12 19:25:53 -0700

    Edited game.vue page text

--- wordle-web/pages/game.vue
+++ wordle-web/pages/game.vue
@@ -3,13 +3,14 @@
     <v-col cols="12" sm="8" md="6">
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Game Page
+          Wardle Game
         </v-card-title>
         <v-card-text>
           <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
         </v-card-text>
         <v-card-actions>
