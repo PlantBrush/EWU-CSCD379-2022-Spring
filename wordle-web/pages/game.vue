d5b1a616a349f61d4a7f88dbbccbcf1c2d326348
PlantBrush <jrobak@ewu.edu>
2022-04-12 19:16:23 -0700

    Updated index.vue

--- wordle-web/pages/game.vue
+++ wordle-web/pages/game.vue
@@ -1,1 +1,32 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to the Game Page
+        </v-card-title>
+        <v-card-text>
+          <p>
+            Vuetify is a progressive Material Design component framework for
+            Vue.js. It was designed to empower developers to create amazing
+            applications.
+          </p>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" nuxt to="/"> Back </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
 
+<script lang="ts">
+import Vue from 'vue'
+import Component from "vue-class-component"
+
+@Component
+export default class GamePage extends Vue{
+  name: string = 'GamePage';
+}
+</script>
